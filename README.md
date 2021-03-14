f56178bd7008689930294325d2d8aba1220545e5
Martín Paradiso <martinparadiso@outlook.com>
2021-03-14 18:34:17 -0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sti-hpc
+
 Simulador de Transmisiones Intrahospitalarias para HPC
+
+## Compiling
+
+### Ubuntu
+
+Install required packages from Ubuntu repositories:
+
+```sh
+$ apt install -y git build-essential cmake clang clang-tidy wget tar bzip2 diffutils zlib1g-dev libgtest-dev
+```
+
+Install the rest of the libraries, optionally pass the number of jobs used to
+compile the libraries:
+
+```sh
+cd lib
+./install.py all [-j J]
+```
--- utils/runner.py
+++ utils/runner.py
@@ -4,29 +4,33 @@ import argparse
 import subprocess
 import time
 
-parser = argparse.ArgumentParser(description='Simulation runner')
-parser.add_argument('-n', '--nodes', default=4)
-parser.add_argument('--mpi', default='/home/martin/repast/mpich/bin/mpiexec')
-parser.add_argument('-e', '--executable', default='./sti-demo')
+parser = argparse.ArgumentParser(
+    description='Simulation runner. Facilitates the execution of the simulation')
+parser.add_argument('-n', '--nodes', default=4,
+                    help='Number of process to run')
+parser.add_argument(
+    '--mpi', default='../lib/mpich/bin/mpiexec', help='MPI executable')
+parser.add_argument('-e', '--executable', default='./sti-demo',
+                    help='Simulation binary file')
 parser.add_argument('--config', default='./config.props')
 parser.add_argument('--model', default='./model.props')
-parser.add_argument('--debug', type=int)
+parser.add_argument('--debug', type=int,
+                    help='Pass the --debug flag to the N process')
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
     commands = [args.mpi,
-                    '-np', str(args.nodes),
-                    f"./{args.executable}",
-                    f"./{args.config}",
-                    f"./{args.model}"]
+                '-np', str(args.nodes),
+                f"./{args.executable}",
+                f"./{args.config}",
+                f"./{args.model}"]
 
     if args.debug:
         commands.append(f"--debug={args.debug}")
 
     print(f"Running: {' '.join(commands)}")
-    
+
     start_time = time.time()
     subprocess.run(commands)
     print(f"Program finished in {time.time() - start_time:.2} seconds")
-
