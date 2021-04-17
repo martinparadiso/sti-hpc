386ad5cb56504808516b6260546c705994cdb62b
Martín Paradiso <martinparadiso@outlook.com>
2021-04-17 17:56:41 -0300

    Checkpoint

--- utils/demo.py
+++ utils/demo.py
@@ -1,14 +1,141 @@
 #!/usr/bin/python3
 
-import plan
+import simulation as s
+import numpy as np
+import random
 
-hospital = plan.BuildingPlan(30,30)
-for i in range(30):
-    hospital.add( (0, i), plan.Wall() )
-    hospital.add( (i, 0), plan.Wall() )
-    hospital.add( (29,i), plan.Wall() )
-    hospital.add( (i,29), plan.Wall() )
+# Create a new simple hospital
 
-hospital.add( (10,10), plan.Doctor(10) )
+hospital = s.Hospital(10, 10)
 
-hospital.plot()
+for i in range(10):
+    hospital.add_element(s.Wall((0, i)))
+    hospital.add_element(s.Wall((9, i)))
+    hospital.add_element(s.Wall((i, 0)))
+    hospital.add_element(s.Wall((i, 9)))
+
+hospital.add_element(s.DoctorOffice('general_practitioner', (3, 8), (3, 7)))
+hospital.add_element(s.DoctorOffice('radiologist', (5, 8), (5, 7)))
+
+hospital.add_element(s.Receptionist((8, 2), (8, 3)))
+
+hospital.add_element(s.Triage((5, 1)))
+hospital.add_element(s.ICU((8, 8)))
+
+for i in range(1, 4, 1):
+    hospital.add_element(s.Chair((i, 2)))
+
+hospital.add_element(s.Entry((0, 5)))
+hospital.add_element(s.Exit((9, 5)))
+
+hospital.parameters = {
+    'objects': {
+        'chair': {
+            'infect_probability': 0.1,
+            'cleaning_interval': s.TimePeriod(0, 2, 0, 0),
+            'radius': 0.1
+        },
+        'bed': {
+            'infect_probability': 0.0,
+            'radius': 0.0,
+            'cleaning_interval': s.TimePeriod(0, 2, 0, 0)
+        }
+    },
+    'icu': {
+        'environment': {
+            'infection_probability': 0.1
+        },
+        'beds': 5,
+        'dead_probability': 0.05,
+        'sleep_times': [
+            {
+                'time': s.TimePeriod(1, 0, 0, 0),
+                'probability': 0.2
+            },
+            {
+                'time': s.TimePeriod(2, 0, 0, 0),
+                'probability': 0.2
+            },
+            {
+                'time': s.TimePeriod(4, 0, 0, 0),
+                'probability': 0.2
+            },
+            {
+                'time': s.TimePeriod(8, 0, 0, 0),
+                'probability': 0.2
+            },
+            {
+                'time': s.TimePeriod(16, 0, 0, 0),
+                'probability': 0.2
+            }
+        ]
+    },
+    'reception': {
+        'attention_time': s.TimePeriod(0, 0, 15, 0)
+    },
+    'triage': {
+        'icu': 0.2,
+        'doctors_probabilities': [
+            {
+                'specialty': 'general_practitioner',
+                'probability': 0.6
+            },
+            {
+                'specialty': 'radiologist',
+                'probability': 0.2
+            }
+        ],
+        'levels': [
+            {
+                'level': 1,
+                'probability': 0.2,
+                'wait_time': s.TimePeriod(0, 0, 0, 0)
+            },
+            {
+                'level': 2,
+                'probability': 0.2,
+                'wait_time': s.TimePeriod(0, 0, 0, 15)
+            },
+            {
+                'level': 3,
+                'probability': 0.2,
+                'wait_time': s.TimePeriod(0, 0, 0, 30)
+            },
+            {
+                'level': 4,
+                'probability': 0.2,
+                'wait_time': s.TimePeriod(0, 0, 1, 0)
+            },
+            {
+                'level': 5,
+                'probability': 0.2,
+                'wait_time': s.TimePeriod(0, 0, 2, 0)
+            }
+        ],
+        'attention_time': s.TimePeriod(0, 0, 15, 0)
+    },
+    'doctors': [
+        {
+            'attention_duration': s.TimePeriod(0, 0, 30, 0),
+            'specialty': 'general_practitioner'
+        },
+        {
+            'attention_duration': s.TimePeriod(0, 1, 0, 0),
+            'specialty': 'radiologist'
+        }
+    ],
+    'patient': {
+        'walk_speed': 0.2,
+        'influx': np.array([[random.randrange(2, 10) for i in range(12)] for j in range(365)]),
+        'infected_probability': 0.1
+    },
+    'human': {
+        'infect_distance': 2.0,
+        'contamination_probability': 0.1,
+        'incubation_time': s.TimePeriod(0, 2, 0, 0),
+        'infect_probability': 0.2
+    }
+}
+hospital.plot().to_console()
+
+# Generate a new run
